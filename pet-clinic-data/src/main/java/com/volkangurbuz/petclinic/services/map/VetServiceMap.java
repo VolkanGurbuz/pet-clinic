e1147fbb5431aa051f9dcb0c4493dbd3d30ec0f7
volkangurbuz <volkangurbuz16@gmail.com>
2020-04-16 17:16:34 +0200

    added list owners

--- pet-clinic-data/src/main/java/com/volkangurbuz/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/volkangurbuz/petclinic/services/map/VetServiceMap.java
@@ -2,9 +2,11 @@ package com.volkangurbuz.petclinic.services.map;
 
 import com.volkangurbuz.petclinic.model.Vet;
 import com.volkangurbuz.petclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
   @Override
   public Set<Vet> findAll() {
