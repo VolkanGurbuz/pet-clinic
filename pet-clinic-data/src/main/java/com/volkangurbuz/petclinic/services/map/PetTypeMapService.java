d05b7c50854170a87fb80a3916127c48c73b51e3
volkangurbuz <volkangurbuz16@gmail.com>
2020-04-18 18:12:42 +0200

    create pettype aand speciality entitity  added

---
+++ pet-clinic-data/src/main/java/com/volkangurbuz/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package com.volkangurbuz.petclinic.services.map;
+
+import com.volkangurbuz.petclinic.model.PetType;
+import com.volkangurbuz.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+  @Override
+  public Set<PetType> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public void deletById(Long aLong) {
+    super.deleteById(aLong);
+  }
+
+  @Override
+  public void delete(PetType object) {
+    super.delete(object);
+  }
+
+  @Override
+  public PetType save(PetType object) {
+    return super.save(object);
+  }
+
+  @Override
+  public PetType findById(Long id) {
+    return super.findById(id);
+  }
+}
